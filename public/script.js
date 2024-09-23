59442ea2d106976138ea31bbcd2981dd7a15b792
Evandro <dam450@duck.com>
2024-09-23 16:23:13 -0300

    chore: add copyright log

--- public/script.js
+++ public/script.js
@@ -1,1 +1,4 @@
-console.log('hello!!!');
+console.log(
+  '%c Copyright (c) 2024 dam450.dev all rights reserved. ',
+  'color: yellow; font-style: italic;font-size: 3em; font-weight: bold; background-color: red;padding: 2px',
+);
